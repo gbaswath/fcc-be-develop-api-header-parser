4c4d9ed33b719311f9fd71c3f7a8947cf6204e19
G B Aswath <gbaswath@zohomail.in>
2022-02-28 17:30:37 +0530

    Who Am I -> Request Header Parser
    1. server.js -> Used req.header["param"] for fetching request headers.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,10 +22,28 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+//Who AM I Router
+app.get("/api/whoami", function (req, res) {
+  console.log("Invoking Who AM I??");
+  //Requested IP
+  let reqIP = req.ip;
+  console.log("Requested IP " + reqIP);
+  //Request Header
+  console.log("Requested Header " + JSON.stringify(req.headers));
+  //Request Language
+  var reqLang = req.headers["accept-language"];
+  console.log("Requested Language " + reqLang);
+  //Request Agent
+  var reqSoftware = req.headers["user-agent"];
+  console.log("Requested Software " + reqSoftware);
+  //Expected Response Data
+  var responseData = { "ipaddress": reqIP, "language": reqLang, "software": reqSoftware };
+  console.log("Prepared Response " + JSON.stringify(responseData));
+  res.json(responseData);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
